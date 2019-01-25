da3b2b6e22b88c2b4c80f4c4b58217fd5173a1b0
tesla-1st-code <firstcode2017@gmail.com>
2019-01-25 17:58:53 +0700

    configure app

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
+import 'reflect-metadata';
+
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as routingControllers from "routing-controllers";
 
+const ENV = require("./env.json")[process.env.NODE_ENV || "development"];
 const CORS = (req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -13,3 +17,18 @@ let app = express();
 app.use(CORS);
 app.use(bodyParser.json({ limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: '50mb' , extended: false }));
+
+routingControllers.useExpressServer(app, {
+    routePrefix: ENV['api_url'],
+    classTransformer: false,
+    controllers: [__dirname + "/controllers/*.js"]
+});
+
+app.listen(ENV["server_port"], err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(`Socpred server is running on ${ENV["server_port"]}`);
+});
